87b51e4132972ac84c52c5f02b23baf56f5eb5d7
Julia Leeman <69856591+julialeeman@users.noreply.github.com>
2025-05-29 00:18:30 -0400

    Making compatible with musicogenic dataset

--- BIDS_converter/BIDS_convert_mac.sh
+++ BIDS_converter/BIDS_convert_mac.sh
@@ -2,76 +2,85 @@
 
 ORIG_DATA_DIR="$HOME/Library/CloudStorage/Box-Box/CoganLab"
 # OUTPUT_DIR="$ORIG_DATA_DIR/BIDS-1.2_GlobalLocal"
-OUTPUT_DIR="$HOME/Desktop/BIDS-1.2_GlobalLocal"
+OUTPUT_DIR="$HOME/Desktop/BIDS-1.6_Musicogenic"
 
-TASKS=("GlobalLocal")
-SUB_IDS=(D57)
+TASKS=("D46_Seizures")
+SUB_IDS=(SEIZURE1 SEIZURE2 SEIZURE3 SEIZURE4 SEIZURE5)
 
 for TASK in "${TASKS[@]}"
 do
-    BIDS_DIR="$OUTPUT_DIR/BIDS"
-    ZIP=false
 
-    if [ -d "$OUTPUT_DIR" ]; then
-        rm -rf "$OUTPUT_DIR"
-    fi
-    mkdir "$OUTPUT_DIR"
+        BIDS_DIR="$OUTPUT_DIR/BIDS"
+        ZIP=false
 
-    if [ -d "$BIDS_DIR" ]; then
-        rm -rf "$BIDS_DIR"
-    fi
+        if [ -d "$OUTPUT_DIR" ]; then
+            rm -rf "$OUTPUT_DIR"
+        fi
+        mkdir "$OUTPUT_DIR"
 
-    TASKLOWER=$(echo "$TASK" | tr '[:upper:]' '[:lower:]')
+        if [ -d "$BIDS_DIR" ]; then
+            rm -rf "$BIDS_DIR"
+        fi
+
+        TASKLOWER=$(echo "$TASK" | tr '[:upper:]' '[:lower:]')
+
 
     for SUB_ID in "${SUB_IDS[@]}"
     do 
-        if [ -d "$OUTPUT_DIR/$SUB_ID" ]; then
-            rm -rf "$OUTPUT_DIR/$SUB_ID"
+
+        
+
+        if [ -d "$OUTPUT_DIR/D46_$SUB_ID" ]; then
+            rm -rf "$OUTPUT_DIR/D46_$SUB_ID"
         fi
-        mkdir -p "$OUTPUT_DIR/$SUB_ID"
+        mkdir -p "$OUTPUT_DIR/D46_$SUB_ID"
 
         # CT scan .nii
-        find "$ORIG_DATA_DIR/ECoG_Recon_Full/$SUB_ID/elec_recon" -name "postimpRaw.nii.gz" -type f -exec cp -v {} "$OUTPUT_DIR/$SUB_ID/${SUB_ID}_CT.nii.gz" \;
-        find "$ORIG_DATA_DIR/ECoG_Recon_Full/$SUB_ID/elec_recon" -regex ".*\($SUB_ID.*CT.*\)\|\(postimpRaw\)\.nii" -type f -exec cp -v {} "$OUTPUT_DIR/$SUB_ID/${SUB_ID}_CT.nii" \;
+        find "$ORIG_DATA_DIR/ECoG_Recon_Full/D46/elec_recon" -name "postimpRaw.nii.gz" -type f -exec cp -v {} "$OUTPUT_DIR/D46_$SUB_ID/D46_${SUB_ID}_CT.nii.gz" \;
+        find "$ORIG_DATA_DIR/ECoG_Recon_Full/D46/elec_recon" -regex ".*\(D46.*CT.*\)\|\(postimpRaw\)\.nii" -type f -exec cp -v {} "$OUTPUT_DIR/D46_$SUB_ID/D46_${SUB_ID}_CT.nii" \;
 
         # Electrode locations .txt
-        find "$(dirname "$ORIG_DATA_DIR")/ECoG_Recon/$SUB_ID/elec_recon" -regex ".*${SUB_ID}_elec_locations_RAS_.*\.txt" -type f -exec cp -v {} "$OUTPUT_DIR/$SUB_ID/" \;
+        find "$(dirname "$ORIG_DATA_DIR")/ECoG_Recon/D46/elec_recon" -regex ".*D46_elec_locations_RAS_.*\.txt" -type f -exec cp -v {} "$OUTPUT_DIR/D46_$SUB_ID/" \;
+
+        # Rename .txt
+        find "$OUTPUT_DIR/D46_$SUB_ID" -type f -regex ".*\.txt" | xargs -n 1 basename | xargs -I{} mv -v "$OUTPUT_DIR/D46_$SUB_ID/{}" "$OUTPUT_DIR/D46_$SUB_ID/D46_${SUB_ID}_elec_locations_RAS_brainshifted.txt"
+
 
         # T1 MRI file .mgz
-        find "$ORIG_DATA_DIR/ECoG_Recon_Full/$SUB_ID/elec_recon" -name "T1.nii.gz" -type f -exec cp -v {} "$OUTPUT_DIR/$SUB_ID/${SUB_ID}_T1w.nii.gz" \;
+        find "$ORIG_DATA_DIR/ECoG_Recon_Full/D46/elec_recon" -name "T1.nii.gz" -type f -exec cp -v {} "$OUTPUT_DIR/D46_$SUB_ID/D46_${SUB_ID}_T1w.nii.gz" \;
 
         # Stim file corrections
-        if [ "$TASK" == "Phoneme_Sequencing" ]; then
-            cp -v "$ORIG_DATA_DIR/ECoG_Task_Data/response_coding/PhonemeSequencingStimStarts.txt" "$OUTPUT_DIR/$SUB_ID/${SUB_ID}_PhonemeSequencingStimStarts.txt"
-        fi
+       # if [ "$TASK" == "Phoneme_Sequencing" ]; then
+       #     cp -v "$ORIG_DATA_DIR/ECoG_Task_Data/response_coding/PhonemeSequencingStimStarts.txt" "$OUTPUT_DIR/D46_$SUB_ID/${SUB_ID}_PhonemeSequencingStimStarts.txt"
+       # fi
 
         # EEG files
-        if ! find "$ORIG_DATA_DIR/D_Data/$TASK/" -iregex ".*$SUB_ID.*\.edf" -exec false {} + ; then
-            find "$ORIG_DATA_DIR/D_Data/$TASK/" -iregex ".*$SUB_ID.*\.edf" -exec cp -v {} "$OUTPUT_DIR/$SUB_ID/" \;
-            find "$OUTPUT_DIR/$SUB_ID/" -regex ".*\.EDF" | xargs -I{} basename -s ".EDF" {} | xargs -I{} mv -v "$OUTPUT_DIR/$SUB_ID/{}.EDF" "$OUTPUT_DIR/$SUB_ID/{}.edf"
+        if ! find "$ORIG_DATA_DIR/D_Data/$TASK/" -iregex ".*_$SUB_ID.*\.edf" -exec false {} + ; then
+            find "$ORIG_DATA_DIR/D_Data/$TASK/" -iregex ".*_$SUB_ID.*\.edf" -exec cp -v {} "$OUTPUT_DIR/D46_$SUB_ID/" \;
+            find "$OUTPUT_DIR/$SUB_ID/" -regex ".*\.EDF" | xargs -I{} basename -s ".EDF" {} | xargs -I{} mv -v "$OUTPUT_DIR/D46_$SUB_ID/{}.EDF" "$OUTPUT_DIR/D46_$SUB_ID/{}.edf"
             if $ZIP ; then
-                find "$ORIG_DATA_DIR/D_Data/$TASK/$SUB_ID" -regex ".*\.\(EDF\)\|\(edf\)" | xargs -I{} basename {} | xargs -I{} cut -f 1 -d '.' {} | xargs -I{} echo "$OUTPUT_DIR/$SUB_ID/{}.edf" | xargs -I{} gzip -6 -v {}
+                find "$ORIG_DATA_DIR/D_Data/$TASK/_$SUB_ID" -regex ".*\.\(EDF\)\|\(edf\)" | xargs -I{} basename {} | xargs -I{} cut -f 1 -d '.' {} | xargs -I{} echo "$OUTPUT_DIR/D46_$SUB_ID/{}.edf" | xargs -I{} gzip -6 -v {}
             fi
         else
-            x=( $(find "$ORIG_DATA_DIR/D_Data/$TASK/$SUB_ID" -regex ".*$SUB_ID.*\.ieeg.dat" -type f | rev | sed -r "s|/|_|" | sed -r "s|/|noisseS/|" | rev | xargs -n 1 basename | sed -r "s|${SUB_ID}_||") )
-            y=( $(find "$ORIG_DATA_DIR/D_Data/$TASK/$SUB_ID" -regex ".*$SUB_ID.*\.ieeg.dat") )
+            x=( $(find "$ORIG_DATA_DIR/D_Data/$TASK/_$SUB_ID" -regex ".*$SUB_ID.*\.ieeg.dat" -type f | rev | sed -r "s|/|_|" | sed -r "s|/|noisseS/|" | rev | xargs -n 1 basename | sed -r "s|${SUB_ID}_||") )
+            y=( $(find "$ORIG_DATA_DIR/D_Data/$TASK/_$SUB_ID" -regex ".*$SUB_ID.*\.ieeg.dat") )
             z=${#x[@]}
             if $ZIP ; then
-                for((i=0;i<=$z-1;i+=1)); do gzip -c -6 -v ${y[$i]} > "$OUTPUT_DIR/$SUB_ID/${SUB_ID}_${x[$i]}.gz"; done
+                for((i=0;i<=$z-1;i+=1)); do gzip -c -6 -v ${y[$i]} > "$OUTPUT_DIR/D46_$SUB_ID/${SUB_ID}_${x[$i]}.gz"; done
             else 
-                for((i=0;i<=$z-1;i+=1)); do rsync -vP ${y[$i]} "$OUTPUT_DIR/$SUB_ID/${SUB_ID}_${x[$i]}"; done
+                for((i=0;i<=$z-1;i+=1)); do rsync -vP ${y[$i]} "$OUTPUT_DIR/D46_$SUB_ID/${SUB_ID}_${x[$i]}"; done
             fi
         fi
 
-        find "$ORIG_DATA_DIR/D_Data/$TASK/$SUB_ID" -regex ".*experiment\.mat" -exec cp -v {} "$OUTPUT_DIR/$SUB_ID/" \;
+        find "$ORIG_DATA_DIR/D_Data/$TASK" -regex ".*experiment\.mat" -exec cp -v {} "$OUTPUT_DIR/D46_$SUB_ID/" \;
 
         # Event .mat
-        find "$ORIG_DATA_DIR/D_Data/$TASK/$SUB_ID" -regex ".*\/mat\/.*[tT]rial[(Info)s].*\.mat" -exec cp -v {} "$OUTPUT_DIR/$SUB_ID/" \;
+        find "$ORIG_DATA_DIR/D_Data/$TASK/$SUB_ID" -regex ".*\/mat\/.*[tT]rial[(Info)s].*\.mat" -exec cp -v {} "$OUTPUT_DIR/D46_$SUB_ID/" \;
 
         # Rename .mat
-        find "$OUTPUT_DIR/$SUB_ID" -type f -regex ".*\.mat" | xargs -n 1 basename | xargs -I{} mv -v "$OUTPUT_DIR/$SUB_ID/{}" "$OUTPUT_DIR/$SUB_ID/${SUB_ID}_{}"
+        find "$OUTPUT_DIR/D46_$SUB_ID" -type f -regex ".*\.mat" | xargs -n 1 basename | xargs -I{} mv -v "$OUTPUT_DIR/D46_$SUB_ID/{}" "$OUTPUT_DIR/D46_$SUB_ID/D46_${SUB_ID}_{}"
 
-        python -m data2bids -c config.json -i "$OUTPUT_DIR/$SUB_ID" -o "$BIDS_DIR" -v || { echo "BIDS conversion for $SUB_ID failed, trying next subject" ; continue; }
+        python -m data2bids -c config.json -i "$OUTPUT_DIR/D46_$SUB_ID" -o "$BIDS_DIR" -v || { echo "BIDS conversion for $SUB_ID failed, trying next subject" ; continue; }
 
         [[ $RAN_SUBS =~ (^| )$SUB_ID( |$) ]] || RAN_SUBS+="$SUB_ID "
         [[ $RAN_TASKS =~ (^| )$TASK( |$) ]] || RAN_TASKS+="$TASK "
